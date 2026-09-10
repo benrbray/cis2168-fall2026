07a19699a9fc97820a3578d8d3073e4aa4e07360
Benjamin R. Bray <benrbray@gmail.com>
2026-09-10 23:38:36 +0900

    cis2168 linked lists

---
+++ src/tuj/cis2168/lecture06/ListExample.java
@@ -0,0 +1,34 @@
+package tuj.cis2168.lecture06;
+
+public class ListExample {
+  public static void main() {
+    Cis2168List<String> example = new SinglyLinkedList<>();
+    example.add("apricot");
+    example.add("banana");
+    example.add("cherry");
+
+    System.out.println("Before");
+    printList(example);
+
+    example.insert(1, "avocado");
+
+    System.out.println("\n\nAfter");
+    printList(example);
+
+    // 0 apricot
+    // 1 avocado
+    // 2 banana
+    // 3 cherry
+
+    example.remove(0);
+    System.out.println("\n\nAfter Removal");
+    printList(example);
+
+  }
+
+  public static <T> void printList(Cis2168List<T> list) {
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println("idx=" + i + ":" + list.get(i));
+    }
+  }
+}
